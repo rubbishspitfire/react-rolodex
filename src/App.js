45f9269278d8fa3c0ef6a3344ca085facfbd3c66
Patience Condell <condellp@gmail.com>
2023-06-24 16:32:04 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -21,20 +21,20 @@ class App extends React.Component {
       .then(users => this.setState({ monsters: users }));
   }
 
-  handleChange = e => this.setState({ searchField: e.target.value });
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
 
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
-        <SearchBox
-          placeholder="Search monsters..."
-          handleChange={this.handleChange}
-        />
+        <SearchBox onSearchChange={this.onSearchChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
